8e32b04130473654e00766bd4383a523f4bd416e
cyjiin <90894850+cyjiin@users.noreply.github.com>
2023-08-22 22:05:23 +0900

    한글 깨짐 방지 & data넘기기
    jsonify 넘길때 한글 깨짐 방지위해서 코드 추가하였습니다. 그리고 추가된 함수들은 수정 필요합니다. 틀만 잡아두었습니다.

--- python_test/mail.py
+++ python_test/mail.py
@@ -10,6 +10,7 @@ mydb = mysql.connector.connect(
 )
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False # 한글 데이터 깨짐 방지
 list=[]
 
 # mail list 함수 -> render_template
@@ -25,6 +26,13 @@ def show_list(id):
 
 print(show_list('ssongsh98@naver.com'))
 
+#메일 목록 넘기기
+@app.route('/send_mail_list')
+def send_mail_list(id):
+    list = show_list(id)
+
+    return jsonify(list)
+    
 
 #메일 축적 함수 -> render_template, database
 def insert_mail_list(id):
@@ -52,6 +60,14 @@ def search_contents(str):
     cur.close()
 
     return list
+
+# 검색 메일 목록 넘기기
+@app.route('/send_search_contents')
+def send_search_contents(str):
+    send_search_list = search_contents(str)
+
+    return jsonify(send_search_list)
+
 
 #검색 정렬 함수
 def search_sort(str):
@@ -104,9 +120,18 @@ def multi_delete(str):
           if sorted_list[x][0] == want_to_delete_list[y]:
              swap_elements(sorted_list, 0, x)
              sorted_list.pop(0)
+
+             count = count + 1
           break
     
+    carbon(count)
     return sorted_list
+
+# 탄소배출량 계산 함수
+def carbon(num):
+    carbon = num * 4
+    print('약', carbon ,'g의 탄소배출을 막으셨어요!')
+
 
 # http protocol routing function
 #@app.route('/', methods = ['GET', 'POST'])
@@ -146,4 +171,3 @@ def hello_html():
 
 if __name__ =="__main__":
     app.run(host = "0.0.0.0", port='8080')
-
